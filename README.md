83cea0bbb242d24e66f0c56ce79fcd56c599423d
Pablo Lara <31992935+PL01@users.noreply.github.com>
2018-04-07 13:37:49 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# GrowWithGoogle_PixelArtMaker
-Pixel Art Mater Lab that I made in Phase 1 of Grow with Google 
+# Pixel Art Maker Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+## Instructions
+
+To get started, open `designs.js` and start building out the app's functionality.
+
+For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
---
+++ designs.js
@@ -0,0 +1,34 @@
+// Select color input - X
+// Select size input - X
+// Make variables for height(row) & width(column). - X
+// Use for loop and inner for loop to make grid. - X
+
+const table = $('#pixelCanvas');//Variable for table
+$(document).ready(function(){
+    $("#sizePicker").submit(function (e){
+        e.preventDefault(); //prevents default setting where the page shows no grid at all.
+        const height = $('#inputHeight').val(); //gridHeight
+        const width = $('#inputWidth').val(); //gridWith
+        makeGrid(height, width);
+    });
+});
+//Function will make grid using height and width as arguments
+function makeGrid(h, w) {
+    table.empty(); //clears table every time a grid is made
+    for (let n = 1; n <= h; n++){ //creates row
+        const row = $('<tr></tr>');
+        table.append(row);
+        for (let m = 1; m <= w; m++){ //creates column
+            const column = $('<td></td>');
+            row.append(column);
+        }
+    }
+};
+
+//Function to add color into a cell
+table.on('click', 'td', function(){
+    let penColor = $('#colorPicker').val(); //Color variable that has the color picker's values
+    $(this).css('background-color', penColor); //sets background color property to penColor's value.
+});
+
+//function to make table fade in and out
